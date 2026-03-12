b104bd434ccf8aec0a82e2ac8c16b0e5f8bfa8fb
duytungqua <quangduy.hcmut@gmail.com>
2026-03-12 15:16:11 +0700

    tut: update some tut in python

---
+++ tutoria/calculator.py
@@ -0,0 +1,6 @@
+num1 = input("Nhap so thu nhat: ")
+num2 = input("Nhap so thu hai: ")
+sum = int(num1) + int(num2)
+
+#input function always return string, so we need to convert it to number before doing calculation
+print(f"Ket qua phep cong {num1} + {num2} = {sum}")
